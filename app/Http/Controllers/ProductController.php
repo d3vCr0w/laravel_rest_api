61add5840a7723f565b4ac1e39d0da10f790b60f
Santiago Moreno <santiagomoreno696@gmail.com>
2019-08-05 21:05:41 -0500

    Commit inicial con creat y eliminar funcionando

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Product;
+use Illuminate\Http\Request;
+use App\Http\Controllers\ApiController;
+
+class ProductController extends ApiController
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $product = Product::all();
+        return $this->sendResponse($product->toArray(),'Listado de productos');
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $product = new Product();
+        $product->nombre =  $request->nombre;
+        $product->save();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Product $product)
+    {
+        return Product::where('id', $product->id)->get();
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Product $product)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Product $product)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Product $product)
+    {
+        //dd($product);
+        //$res=Product::where('id',$product->id)->delete();
+        $res = Product::destroy($product->id);
+        if ($res) {
+            return response()->json([
+                'status' => '1',
+                'msg' => 'success'
+            ]);
+        } else {
+            return response()->json([
+                'status' => '0',
+                'msg' => 'fail'
+            ]);
+        }
+    }
+}
